318a0a84c6c92c0fcf05519d1f730a076cfb5dc0
khaled39ab <khaled39ab@gmail.com>
2022-07-18 01:23:16 +0600

    write step for setup

--- __steps.js
+++ __steps.js
@@ -1,9 +1,13 @@
 /**
+ * -----------------------------
+ * Basic server setup
+ * -----------------------------
  * 1- create a folder (manually or mkdir)
  * 2- open command line to that folder
  * 3- use npm init command
  * 4- npm install express
  * 5- create index.js file
+ * 
  * -----------------------------
  * for express
  * -----------------------------
@@ -14,4 +18,16 @@
  * 5- listen to port
  * 6- use node index.js command
  * 7- Go to browser and type port (localhost:5000)
+ * 
+ * ------------------------------
+ * Get
+ * ------------------------------
+ * 1- cors middleware
+ * 2- 
+ * 
+ * --------------------------------
+ * Post
+ * --------------------------------
+ * 1- express middleware
+ * 
  **/
